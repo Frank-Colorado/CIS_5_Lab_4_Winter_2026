08548d11ba4b1a6e1792ba2d46626518ffa24f78
Frank Colorado <chawnkiee@gmail.com>
2026-02-02 15:40:25 -0800

    main function
    main function with basic setup

---
+++ Lab_04_Functions&PassingMethods/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using std::cout;
+
+
+
+
+int main() {
+
+	// Constant variable for array size
+	int const SIZE = 5;
+
+	// Array holding all student scores
+	int scores[SIZE] = { 65, 82, 90, 45, 78 };
+
+
+	return 0;
+}

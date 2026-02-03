8de2925db5d73074ef0e57203a09e59fc7aba5f4
Frank Colorado <chawnkiee@gmail.com>
2026-02-02 16:24:37 -0800

    addBonus function
    Created a function to add bonus points to the students scores. Modifying the original array

--- Lab_04_Functions&PassingMethods/main.cpp
+++ Lab_04_Functions&PassingMethods/main.cpp
@@ -32,6 +32,25 @@ static double getAverage(int sum, int size) {
 	return static_cast<double>(sum) / size;
 }
 
+// Function to add bonus points to each student's score
+// This function will use a pointer to the array to modify the original scores
+// This function takes 3 parameters:
+// 1. A pointer to an array of integers representing student scores
+// 2. An integer representing the size of the array
+// 3. An integer representing the bonus points to be added
+static void addBonus(int(*scores)[5], int size, int bonus) {
+
+	// Check to make sure the pointer is not null
+	if (scores == nullptr) return; // If the pointer is null, simply return
+	
+	// Here I use a classic for loop to iterate through the array and add the bonus points to each score
+	for (int i = 0; i < size; ++i) {
+		(*scores)[i] += bonus;
+	}
+}
+
+
+
 int main() {
 
 	// Constant variable for array size
@@ -40,11 +59,36 @@ int main() {
 	// Array holding all student scores
 	int scores[SIZE] = { 65, 82, 90, 45, 78 };
 
+	// Variable for holding the bonus points to be added to each score
+	int bonus = 5;
+
+	// Pointer to the scores array
+	int (*ptrScores)[SIZE] = &scores;
+
+
 	// Calculate the sum of all student scores
 	int totalScore = calculateSum(scores, SIZE);
 
 	// Then I use the sum that I got from the calculateSum function to get the average score
 	int averageScore = getAverage(totalScore, SIZE);
+
+	// Output of all the current scores 
+	cout << "Current Scores: ";
+	for (int i = 0; i < SIZE; ++i) {
+		cout << scores[i] << " ";
+	}
+	// Function to add bonus points to each student's score
+	addBonus(&scores, SIZE, bonus);
+
+	// Output of all the new scores after adding bonus points
+	cout << "\nNew Scores after adding bonus points: ";
+	for (int i = 0; i < SIZE; ++i) {
+		cout << scores[i] << " ";
+	}
+
+	
+
+
 
 
 
